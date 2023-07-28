9982a9c871fa81189d17a190b8de3cf8fc1a140d
DanmoSAMA <772523546@qq.com>
2023-07-28 17:27:31 +0800

    实现 shallowReadonly

--- reactivity/baseHandler.ts
+++ reactivity/baseHandler.ts
@@ -1,23 +1,29 @@
-import { isObject } from '../shared';
+import { extend, isObject } from '../shared';
 import { track, trigger } from './effect';
 import { ReactiveFlags, reactive, readonly } from './reactive';
 
 const get = createGetter();
 const set = createSetter();
 const readonlyGet = createGetter(true);
+const shallowReadonlyGet = createGetter(true, true);
 
-function createGetter(isReadonly = false) {
+function createGetter(isReadonly = false, shallow = false) {
   return function (target, key) {
     if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly;
     } else if (key === ReactiveFlags.IS_READONLY) {
       return isReadonly;
     }
+
+    const res = Reflect.get(target, key);
+
+    if (shallow) {
+      return res;
+    }
     // 依赖收集
     if (!isReadonly) {
       track(target, key);
     }
-    const res = Reflect.get(target, key);
 
     if (isObject(res)) {
       return isReadonly ? readonly(res) : reactive(res);
@@ -47,3 +53,7 @@ export const readonlyHandlers = {
     return true;
   }
 };
+
+export const shallowReadonlyHandlers = extend({}, readonlyHandlers, {
+  get: shallowReadonlyGet
+});
