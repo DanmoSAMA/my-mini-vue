ef7ab319e5e3f2afda1158a93b7c0436602d06fa
DanmoSAMA <772523546@qq.com>
2023-08-04 22:36:00 +0800

    实现解析 element 标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast';
 
+const enum TagType {
+  Start,
+  End
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content);
   return createRoot(parseChildren(context));
@@ -13,10 +18,16 @@ function createParserContext(content: string): any {
 
 function parseChildren(context) {
   const nodes: any = [];
+  const s = context.source;
 
   let node;
-  if (context.source.startsWith('{{')) {
+
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context);
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
 
   nodes.push(node);
@@ -62,6 +73,33 @@ function parseInterpolation(context) {
       type: NodeTypes.SIMPLE_EXPRESSION,
       content: content
     }
+  };
+}
+
+function parseElement(context) {
+  // <div></div>
+  //  =>
+  // return {
+  //   type: NodeTypes.ELEMENT,
+  //   tag: 'div'
+  // };
+
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+  return element;
+}
+
+function parseTag(context, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
   };
 }
 
